1384485934a86ca0a570025d3632a08f4117bc27
LapekPiotrek\Pioter <pss95@tlen.pl>
2017-05-23 10:55:36 +0200

    Added window :D

--- Program.cs
+++ Program.cs
@@ -3,17 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 using TrafficLightsControlSystem;
 
 namespace TraffiCProject
 {
     class Program
     {
+        [STAThread]
         static void Main(string[] args)
         {
-            TrafficSimulation simulation = new TrafficSimulation(4);
-            simulation.StartSimulation();
-            Console.Read();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new TrafficSim());
+            // TrafficSimulation simulation = new TrafficSimulation(4);
+            // simulation.StartSimulation();
+            // Console.Read();
         }
     }
 }
