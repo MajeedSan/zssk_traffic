f8d7a61087b2a887620e9088f282a7a422c339f6
grzegorz.luszczek <luszczek.grzegorz@gmail.com>
2017-06-02 23:15:11 +0200

    remove conflict headers

--- Program.cs
+++ Program.cs
@@ -3,11 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-<<<<<<< HEAD
 using System.Windows.Forms;
-=======
 using System.Timers;
->>>>>>> 5e85db72f10760f68afe9ae308cdab6d0180c062
 using TrafficLightsControlSystem;
 
 namespace TraffiCProject
@@ -17,13 +14,8 @@ namespace TraffiCProject
         [STAThread]
         static void Main(string[] args)
         {
-<<<<<<< HEAD
-            TrafficSimulation simulation = new TrafficSimulation(1);
-=======
-           
-            TrafficSimulation simulation = new TrafficSimulation(1);
+            TrafficSimulation simulation = new TrafficSimulation(4);
             var watch = System.Diagnostics.Stopwatch.StartNew();
->>>>>>> 5e85db72f10760f68afe9ae308cdab6d0180c062
             simulation.StartSimulation();
             var elapsedMs = watch.ElapsedMilliseconds;
             Console.WriteLine($"Time: {elapsedMs}");
